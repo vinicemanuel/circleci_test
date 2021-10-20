f70c8de2aded1814f4e2bc486f56d3da6d7fe8a2
vinicius emanuel <vncemanuel@gmail.com>
2021-10-20 20:39:52 -0300

    Initial commit

---
+++ circleci_test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  circleci_test
+//
+//  Created by Vinicius Silva on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
